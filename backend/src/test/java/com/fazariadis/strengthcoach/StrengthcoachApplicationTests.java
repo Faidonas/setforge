0c2967b21a53e9bbdc81cca765d6fd1d1583d0e4
Faidonas <ff.azariadis@gmail.com>
2026-09-12 20:35:25 +0300

    Initialize Spring Boot backend

---
+++ backend/src/test/java/com/fazariadis/strengthcoach/StrengthcoachApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fazariadis.strengthcoach;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrengthcoachApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
