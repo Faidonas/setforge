348dc7fb529dbde7b83e6cc4f30ba5bd7f4ba829
Faidonas <ff.azariadis@gmail.com>
2026-09-13 18:47:43 +0300

    init mobile set up

--- backend/src/main/java/com/fazariadis/strengthcoach/StrengthCoachApplication.java
+++ backend/src/main/java/com/fazariadis/strengthcoach/StrengthCoachApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StrengthcoachApplication {
+public class StrengthCoachApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(StrengthcoachApplication.class, args);
+		SpringApplication.run(StrengthCoachApplication.class, args);
 	}
 
 }
